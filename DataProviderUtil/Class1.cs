96a1da9542ee4a6214029623cc830f7c3f383e48
wingsziye <wingsziye@hotmail.com>
2020-06-13 22:16:46 +0800

    Update First project

---
+++ DataProviderUtil/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreeSql.Sqlite;
+using FreeSql.Extensions;
+
+namespace DataProviderUtil
+{
+    public class Db2Code
+    {
+
+        public Db2Code()
+        {
+            
+        }
+    }
+}
